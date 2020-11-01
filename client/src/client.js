10cd57fb4177cb9127e89757699a37c4abdb2705
Kyle Hoff <kylehoff01@gmail.com>
2020-10-31 21:50:38 -0600

    #3: Initial httpClient setup. GET requests setup and validated on GET boards call

---
+++ client/src/client.js
@@ -0,0 +1,10 @@
+const parser = require('uri-template');
+const axios = require('axios');
+
+
+export function get(url, parameters, config) {
+  var template = parser.parse(url);
+  var expandedUrl = template.expand(parameters);
+
+  return axios.get(expandedUrl, config);
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import { getBoards, getBoard } from './thulloApi';
+
+getBoards()
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+      <h1>Hello</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
