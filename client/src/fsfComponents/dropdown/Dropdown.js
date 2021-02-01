f79faee27d0d4249a515212571bf310d026cc305
dunbheo <kylehoff01@gmail.com>
2021-01-31 17:10:22 -0700

    Adding outside click close

--- client/src/fsfComponents/dropdown/Dropdown.js
+++ client/src/fsfComponents/dropdown/Dropdown.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import caret from './caret-down.svg';
 import styles from './Dropdown.module.css';
 
 const Dropdown = ({header, options, disabled, ...others}) => {
+  const node = useRef();
+
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = e => {
+    if (node.current.contains(e.target)) {
+      // inside click
+      return;
+    }
+    // outside click
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+    };
+  }, []);
 
   const disabledClass = disabled ? styles.disabled : "";
   console.log(disabledClass)
 
   return (
-    <div style={{width: '200px'}}>
+    <div style={{width: '200px'}} ref={node}>
       <div
         className={`${styles.headerContainer} ${disabledClass}`}
         onClick={() => setIsOpen(!isOpen)}
