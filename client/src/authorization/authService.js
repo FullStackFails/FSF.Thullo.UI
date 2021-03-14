6910f07a4e5d1add6de36eaf88503ea0042197da
Patrick Clover <pcloverdevelopment@outlook.com>
2021-03-14 14:59:01 -0600

    Removed console statement

--- client/src/authorization/authService.js
+++ client/src/authorization/authService.js
@@ -1,4 +1,4 @@
-import {UserManager} from 'oidc-client';
+import { UserManager } from "oidc-client";
 
 // Configuration for Oidc
 var config = {
@@ -6,18 +6,17 @@ var config = {
   client_id: "js",
   redirect_uri: "http://localhost:3000/callback",
   response_type: "code",
-  scope:"openid profile thulloApi",
-  post_logout_redirect_uri : "http://localhost:3000",
+  scope: "openid profile thulloApi",
+  post_logout_redirect_uri: "http://localhost:3000",
 };
 export var userManager = new UserManager(config);
 
 export async function isUserLoggedIn() {
   return await userManager.getUser().then(function (user) {
     if (user) {
-        return true;
-    }
-    else {
-        return false;
+      return true;
+    } else {
+      return false;
     }
   });
 }
@@ -29,28 +28,35 @@ export function login() {
 
 export function logout() {
   // Need to remove Access token as default header for axios here
-  return userManager.signoutRedirect().then(() => true).catch(() => false);
+  return userManager
+    .signoutRedirect()
+    .then(() => true)
+    .catch(() => false);
 }
 
 export async function redirectCallBack() {
-  return new UserManager({response_mode:"query"}).signinRedirectCallback().then(function() {
-    window.history.replaceState({},
-      window.document.title,
-      window.location.origin + window.location.pathname);
-    // window.location = "";
-    return true;
-}).catch(function(e) {
-    console.error(e);
-    return false;
-});
+  return new UserManager({ response_mode: "query" })
+    .signinRedirectCallback()
+    .then(function () {
+      window.history.replaceState(
+        {},
+        window.document.title,
+        window.location.origin + window.location.pathname
+      );
+      // window.location = "";
+      return true;
+    })
+    .catch(function (e) {
+      return false;
+    });
 }
 
 export async function getUserAccessToken() {
   const user = await userManager.getUser();
   const accessToken = user.access_token;
   console.log(accessToken);
-  
-  if (!accessToken) return '';
+
+  if (!accessToken) return "";
 
   return accessToken;
 }
