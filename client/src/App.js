aec12faebfa76123762bd6d881ec31630bf9236a
Patrick Clover <pcloverdevelopment@outlook.com>
2021-03-25 18:23:07 -0600

    Updated UI to use react-bootsrap - issue #20

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import {isUserLoggedIn} from './authorization/authService';
+import { isUserLoggedIn } from "./authorization/authService";
 
-import Home from './views/home';
-import Callback from './authorization/callback';
-import Login from './authorization/login'
-import Logout from './authorization/logout'
+import Home from "./views/home";
+import Callback from "./authorization/callback";
+import Login from "./authorization/login";
+import Logout from "./authorization/logout";
 
-import Button from './fsfComponents/button/Button';
+import Button from "react-bootstrap/Button";
 
-import { getBoards } from './api/thulloApi';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const SecuredRoutes = () => {
   return (
     <>
       <Route path="/logout">
-          <Logout />
+        <Logout />
       </Route>
     </>
-  )
-}
+  );
+};
 
-const UnSecuredLinks = () => {
+const UnsecuredLinks = () => {
   return (
     <>
       <Link to="/login">
-        <Button>Login</Button>
+        <Button variant="primary">Login</Button>
       </Link>
       <Link to="/">
-        <Button type="primary">Register</Button>
+        <Button variant="outline-primary">Register</Button>
       </Link>
     </>
-  )
-}
+  );
+};
 
-const SecuredLinks = ({history}) => {
+const SecuredLinks = ({ history }) => {
   return (
     <>
       <Link to="/logout">
-        <Button>Logout</Button>
+        <Button variant="outline-primary">Logout</Button>
       </Link>
     </>
-  )
-}
+  );
+};
 
 function App() {
   const [isLoggedOn, setIsLoggedOn] = useState(false);
@@ -61,32 +54,32 @@ function App() {
       setIsLoggedOn(response);
     }
     isLoggedIn();
-  }, [])
+  }, []);
 
   return (
     <Router>
-    <div>
-      <nav>
-        {/* <Link to="/">
+      <div>
+        <nav>
+          {/* <Link to="/">
           <Button type="primary">Home</Button>
         </Link> */}
-        {isLoggedOn ? <SecuredLinks /> : <UnSecuredLinks />}
-      </nav>
+          {isLoggedOn ? <SecuredLinks /> : <UnsecuredLinks />}
+        </nav>
 
-      <Switch>
-        {isLoggedOn && <SecuredRoutes />}
-        <Route path="/callback">
-          <Callback />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Home actions={{setIsLoggedOn}}/>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+        <Switch>
+          {isLoggedOn && <SecuredRoutes />}
+          <Route path="/callback">
+            <Callback />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home actions={{ setIsLoggedOn }} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
